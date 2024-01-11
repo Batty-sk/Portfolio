60d0c50e9588f54602f77405718cb2d0b9f28cca
Batty-sk <saorav.skumar@gmail.com>
2024-01-11 17:17:41 +0530

    added navigation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+<header>
+<div className='bg-blue-100 w-full h-10 rounded-s-sm rounded-e-lg'>
+  <ul className='p-5 flex justify-around items-center m-0 '>
+    <li className='rounded-full w-10 h-10 bg-blue-700'></li>
+    <li className='rounded-full w-10 h-10 bg-blue-700'></li>
+    <li className='rounded-full w-10 h-10 bg-blue-700'></li>
+    <li className='rounded-full w-10 h-10 bg-blue-700'></li>
+  </ul>
+</div>
+</header>  
+)
 }
 
 export default Header
--- src/app/components/section/Hero.tsx
+++ src/app/components/section/Hero.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='text-center flex-col'>
-
         <h1>
             Saurav Kumar
         </h1>
         <p>
             Full Stack Developer
         </p>
+        
     </div>
 
   )
