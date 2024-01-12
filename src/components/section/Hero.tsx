49f4b42fccd940b6e1160d2403230a4ca9fe3b51
Batty-sk <saorav.skumar@gmail.com>
2024-01-12 17:08:15 +0530

    fixed positions of sea

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -1,15 +1,24 @@
 "use client"
 import React from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-
+import sea from "../../assests/sea.png"
+import sun from "../../assests/sun.png"
+import Image from 'next/image'
 
 const Hero = () => {
 
 
   return (
       <>
-      <ParallaxLayer offset={0} speed={2.5}  >
-        <h1>Hero Section</h1>
+      <ParallaxLayer offset={0} speed={-2} factor={1}>
+      <Image src={sun.src} alt="sun" width={100} height={1} className='z-10'></Image>
+
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={1.5} factor={1}>
+        <div className='h-screen w-screen flex justify-center items-end'>
+        <Image src={sea.src} alt="sea" width={10000} height={1} className='z-15'></Image>
+
+      </div>
       </ParallaxLayer>
   
     </>
--- src/components/section/Main.tsx
+++ src/components/section/Main.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import React from 'react'
 import Hero from './Hero'
