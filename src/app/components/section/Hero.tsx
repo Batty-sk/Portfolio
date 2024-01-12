6314223f2b957120527043eb755178c22f4d0a2d
Batty-sk <saorav.skumar@gmail.com>
2024-01-12 16:22:43 +0530

    main bar

--- src/app/components/section/Hero.tsx
+++ src/app/components/section/Hero.tsx
@@ -8,16 +8,9 @@ const Hero = () => {
 
   return (
       <>
-    <Parallax  pages={2} style={{ top: '0', left: '0' }} >
       <ParallaxLayer offset={0} speed={2.5}  >
-        <h1>Parallax</h1>
+        <h1>Hero Section</h1>
       </ParallaxLayer>
-
-
-      <ParallaxLayer offset={1} speed={2.5}  >
-        <h1>Projects</h1>
-      </ParallaxLayer>
-    </Parallax>
   
     </>
       
---
+++ src/app/components/section/Main.tsx
@@ -0,0 +1,12 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import React from 'react'
+import Hero from './Hero'
+function Main() {
+  return (
+    <Parallax  pages={2} style={{ top: '0', left: '0' }} >
+        <Hero />
+        </Parallax>
+  )
+}
+
+export default Main
