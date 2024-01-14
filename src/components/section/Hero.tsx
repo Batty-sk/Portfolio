3df90a3abf64589642d9e8c0a4a18387f6e14f5a
Batty-sk <saorav.skumar@gmail.com>
2024-01-14 19:03:30 +0530

    updated portfolio

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -15,6 +15,11 @@ const Hero = () => {
       <Image src={sun.src} alt="sun" width={180} height={1} className='z-10' id='sun'></Image>
 
       </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={2} style={{left:'20%', top:'12%',zIndex:'1000'}}>
+        <div >
+          <h1 className='' style={{display:'relative',fontSize:'150px', fontWeight:'bolder',zIndex:'1000'}}>Saurav Kumar</h1>
+        </div>
+      </ParallaxLayer>
       <ParallaxLayer offset={0} speed={1.5} >
         <div className='h-screen w-screen flex justify-center items-end'>
         <Image src={sea.src} alt="sea" width={10000} height={1} className='z-15 flex sea '></Image>
