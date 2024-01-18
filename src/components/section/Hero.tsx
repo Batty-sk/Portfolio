251354bb7ce97d37acf3fb007dd880b3c285be59
Batty-sk <saorav.skumar@gmail.com>
2024-01-18 14:59:26 +0530

    removes borders

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -17,17 +17,17 @@ const Hero = () => {
 
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={2} style={{top:0,bottom:0,left:0,right:0,border:'2px solid black',zIndex:9}} >
+      <ParallaxLayer offset={0} speed={2} style={{top:0,bottom:0,left:0,right:0,zIndex:9}} >
         {/* Content you want to move left and right */}
         <div className='flex  h-[100%] justify-center items-center'>
-        <h1 className='text-9xl text-center border font-extrabold '>
+        <h1 className='text-9xl text-center  font-extrabold '>
           Hi,
         </h1>
-        <h1 className='text-8xl text-center border font-extrabold'>Im Saurav Kumar</h1>
+        <h1 className='text-8xl text-center  font-extrabold'>Im Saurav Kumar</h1>
         </div>
       </ParallaxLayer>
 
-         <ParallaxLayer offset={0.5} speed={1.5} style={{top:0,bottom:0,border:'5px solid red',zIndex:'10'}} id='sea'>
+         <ParallaxLayer offset={0.5} speed={1.5} style={{top:0,bottom:0,zIndex:'10'}} id='sea'>
         <div className='flex h-[100%] justify-center items-center br wave-animation ' id='sea-img' style={{backgroundImage:`url(${sea.src})`,backgroundAttachment:'scroll',backgroundRepeat:'no-repeat', }} >
        </div>
       </ParallaxLayer>
