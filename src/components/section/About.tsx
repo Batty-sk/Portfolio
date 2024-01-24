b31840fa771651ad9f8c99113640c7b0632d040f
Batty-sk <saorav.skumar@gmail.com>
2024-01-24 19:34:07 +0530

    added on-section navigation background changer

--- src/components/section/About.tsx
+++ src/components/section/About.tsx
@@ -8,9 +8,11 @@ import Link from 'next/link'
 
 const About = () => {
   return (
-    <ParallaxLayer offset={1} speed={1} style={{top:0,bottom:0,left:0,right:0}} >
-
+    <ParallaxLayer offset={1} speed={1} style={{top:0,bottom:0,left:0,right:0}}>
         <div className='bg-yellow-100 h-[100%] font-extrabold text-center flex  flex-wrap justify-between relative'>
+         <div className='w-[100%] font-mono'>
+          <h1 className='text-5xl text-yellow-900'>About Me</h1>
+         </div>
           <div className='frame'>
 
           </div>
@@ -19,7 +21,7 @@ const About = () => {
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, natus, alias modi at sapiente obcaecati libero saepe repudiandae odit dolorem enim ullam temporibus incidunt sequi, consequatur iste! Nobis neque deserunt exercitationem, dolor doloribus deleniti ratione, pariatur commodi hic quisquam temporibus labore assumenda amet distinctio incidunt voluptas, ipsa inventore quaerat officia! Accusantium porro dolor officia, eligendi id explicabo pariatur deleniti impedit cumque delectus numquam inventore quibusdam corrupti officiis quo, repellendus consectetur neque eos, voluptate culpa corporis odit ad? Voluptatum, magnam suscipit corporis similique fugit tenetur eligendi, placeat nostrum labore quas fuga dolorum! Voluptates ratione voluptatum vel itaque nihil, suscipit rem! Animi.</p>
           </div>
           <div className='flex flex-col justify-center font-mono w-[100%]'>
-            <h2 className='text-5xl'>Follow Me On </h2>
+            <h2 className='text-3xl'>Follow Me On </h2>
             <div className='flex justify-center mt-3'>
               <Link href={'https://github.com/Batty-sk'} target='_blank' className='me-2'><Image src={Github.src} alt='Github' width={64} height={64}></Image></Link>
               <Link href={'https://linkedin.com/in/saurav-kumar-5225a2292'} target='_blank'> <Image src={Linkedin.src} alt='Linkedin' width={64} height={64}></Image></Link>
--- src/components/section/Main.tsx
+++ src/components/section/Main.tsx
@@ -1,17 +1,53 @@
 "use client"
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import React from 'react'
+import {useState,useEffect,useRef} from 'react'
 import Hero from './Hero'
 import Project from './Project'
 import About from './About'
+
 function Main() {
+  const [isAboutSectionVisible, setIsAboutSectionVisible] = useState(false);
+  const aboutSectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      console.log('yes sir broothererere.')
+      const aboutSectionEntry = entries.find(entry => entry.target === aboutSectionRef.current);
+      
+      if (aboutSectionEntry) {
+        setIsAboutSectionVisible(aboutSectionEntry.isIntersecting);
+      }
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5, // Adjust the threshold based on your needs
+    });
+
+    if (aboutSectionRef.current) {
+      observer.observe(aboutSectionRef.current);
+      console.log('first step done!')
+    }
+
+    return () => {
+      if (aboutSectionRef.current) {
+        observer.unobserve(aboutSectionRef.current);
+      }
+    };
+  }, []);
+
   return (
     <Parallax  pages={3} style={{ top: '0', left: '0', height:'100%' }}  config={{ tension: 150, friction: 30 }}>
       <div className='relative flex items-start' style={{background:'aliceblue'}}>
         <div style={{background:'aliceblue'}}>
         <Hero />
         </div>
-        <About />
+
+        <div ref={aboutSectionRef}>
+        <About  />
+        </div>
          <Project/> 
         </div>  
         </Parallax>
