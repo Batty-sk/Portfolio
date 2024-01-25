8671c77990fa7385a273d82d12673987aada7c5f
Batty-sk <saorav.skumar@gmail.com>
2024-01-25 11:05:18 +0530

    added wooden scrollbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = (section:prop) => {
   return (
 <header className='relative md:h-0 h-48' style={{zIndex:10000}}>
 <div className=' w-full flex md:justify-end justify-end rounded-s-sm rounded-e-lg mt-1 '>
-  <ul className='p-5 flex md:flex-row flex-col md:justify-around justify-end md:items-center items-end me-5'>
+  <ul className='p-5 flex md:flex-row flex-col md:justify-around justify-end md:items-center items-end me-5 m-0'>
     {[1,2,3,4].map((x,i)=>{
       if (x==section.section){
     return <li className='me-5 mt-2 cursor-pointer h-7 w-7 bg-yellow-400 rounded-full box-shadow ' key={i}></li>
