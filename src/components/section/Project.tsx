af27e6fc9cbc1bd6d87d476896e68d06f77f5577
Batty-sk <saorav.skumar@gmail.com>
2024-01-25 12:17:34 +0530

    project section

--- src/components/section/Project.tsx
+++ src/components/section/Project.tsx
@@ -4,8 +4,8 @@ import { ParallaxLayer } from '@react-spring/parallax'
 function Project() {
   return (
     <ParallaxLayer offset={2} speed={1} style={{top:0,bottom:0,left:0,right:0}} >
-        <div className='bg-slate-300 '>
-            <h1> Projects</h1>
+        <div className='bg-[#13553D] h-[100%] '>
+            <h1 className='font-mono text-center text-6xl text-white p-4 font-bold'> Projects</h1>
         </div>
         </ParallaxLayer>
       )
