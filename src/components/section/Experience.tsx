584cdacb84280a8dad821004b709366ec3e8e190
Batty-sk <saorav.skumar@gmail.com>
2024-01-27 16:15:34 +0530

    added expereicne

--- src/components/section/Experience.tsx
+++ src/components/section/Experience.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { ParallaxLayer } from '@react-spring/parallax'
+import { Grid } from '@mui/material'
+import './Experience.css'
 
 function Experience() {
   return (
@@ -7,16 +9,18 @@ function Experience() {
         <div className='bg-[#13553D] h-[100%] '>
             <h1 className='font-mono text-center text-6xl text-white p-4 font-bold'> Experience</h1>
 
-            <div className='flex h-[100%] justify-evenly '>
-              <div>
-                <h3 className='text-white font-mono'>Full Stack Intern </h3>
-              </div>
-                <ul>
-                  <li></li>
-                  <li></li>
-                </ul>
+          <Grid container justifyContent={'space-evenly'} margin={2}>
 
+          <Grid item sm={4}>
+            <div className=''>
+              <h3 className='font-mono text-center xp-title text-white text-2xl'>Full Stack Intern</h3>
             </div>
+          </Grid>
+          <Grid item sm={6}>
+            
+          </Grid>
+
+          </Grid>
         </div>
         </ParallaxLayer>
       )
