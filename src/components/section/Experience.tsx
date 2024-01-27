6452925a8b8cc26d8d01013b382092d8ca48c017
Batty-sk <saorav.skumar@gmail.com>
2024-01-27 15:58:28 +0530

    added material ui

--- src/components/section/Experience.tsx
+++ src/components/section/Experience.tsx
@@ -6,6 +6,17 @@ function Experience() {
     <ParallaxLayer offset={2} speed={1} style={{top:0,bottom:0,left:0,right:0}} >
         <div className='bg-[#13553D] h-[100%] '>
             <h1 className='font-mono text-center text-6xl text-white p-4 font-bold'> Experience</h1>
+
+            <div className='flex h-[100%] justify-evenly '>
+              <div>
+                <h3 className='text-white font-mono'>Full Stack Intern </h3>
+              </div>
+                <ul>
+                  <li></li>
+                  <li></li>
+                </ul>
+
+            </div>
         </div>
         </ParallaxLayer>
       )
