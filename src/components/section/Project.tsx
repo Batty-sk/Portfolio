ad2214883d584e671478a75aa08b0b3d71a12b50
Batty-sk <saorav.skumar@gmail.com>
2024-01-31 13:07:21 +0530

    added new theme

--- src/components/section/Project.tsx
+++ src/components/section/Project.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { ParallaxLayer } from '@react-spring/parallax'
-import night_sky from '../../assests/project_background.png'
-import whale from '../../assests/whale.png'
+import me from '../../assests/me_in_space.png'
 import Image from 'next/image'
+import './Project.css'
 
 const Project = () => {
   return (
     <>
     <ParallaxLayer offset={3} speed={1} style={{top:0,bottom:0,left:0,right:0}} >
-    <div style={{backgroundImage:`url(${night_sky.src})`,backgroundRepeat:'no-repeat',backgroundSize:'cover', height:'100%',backgroundPositionY:'Bottom' }}>Project</div>
+    <div style={{background:'black',backgroundRepeat:'no-repeat',backgroundSize:'cover', height:'100%',backgroundPositionY:'Bottom' }}>Project</div>
     </ParallaxLayer>
-    <ParallaxLayer offset={3} speed={0.5} style={{right:0,bottom:50,top:50}}>
-        <Image src={whale.src} alt='whale'  height={500} width={500}/>
+    <ParallaxLayer offset={3} speed={0.5} style={{top:0,bottom:0,left:0,right:0}} >
+      <Image  src={me.src} height={50} width={100} alt='logo' className='animate-spin-float'/>
+      
     </ParallaxLayer>
+    
+
     </>
 
   )
