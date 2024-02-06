832f7d7e15447769137ccd16cf741c0d7765ef17
Batty-sk <saorav.skumar@gmail.com>
2024-02-06 17:19:30 +0530

    added project

--- src/components/section/Project_struct.tsx
+++ src/components/section/Project_struct.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import project_cineflix from '../../assests/project_cineflix.png'
 
 
 const Project_struct = () => {
   return (
     <div className='flex flex-col'>
-        <div className='flex'>
-            
+        <div className='flex justify-between'>
+      <div style={{boxShadow:'0px 0px 8px white  '}}><Image src={project_cineflix.src} height={1} width={350} alt='' />
+       </div>
+        <h3 className='text-white font-mono '>Movie Companion App</h3>
         </div>
         <div className='text-white'>Description</div>
     </div>
