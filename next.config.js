ef62d5f85aff998f20c5874374d43c9665be5780
Batty-sk <saorav.skumar@gmail.com>
2024-10-04 14:47:16 +0530

    bre

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
-// next.config.js
-module.exports = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(mp3|wav)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-          },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(mp3|wav)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          // Simplified naming pattern
+          name: 'static/media/[name].[hash].[ext]',
+          // Ensure proper path for client-side
+          publicPath: '/_next',
+          // Only emit files when building for client
+          emitFile: !isServer,
         },
-      });
-  
-      return config;
-    },
-  };
-  
+      },
+    });
+
+    return config;
+  },
+  // Add some performance optimizations
+  poweredByHeader: false,
+  reactStrictMode: true,
+  // Increase build timeout if needed
+  staticPageGenerationTimeout: 180,
+};
+
+module.exports = nextConfig;
